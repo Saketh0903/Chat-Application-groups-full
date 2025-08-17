f44cf037fea53b64ce0ab2677ba3d2999158bdab
Saketh0903 <sakethkarumudi@gmail.com>
2025-08-17 21:22:02 +0530

    commit

--- server/src/index.js
+++ server/src/index.js
@@ -39,10 +39,10 @@ app.use("/api/groups", groupRoutes);
 
 // ----------------- STATIC FRONTEND -----------------
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
